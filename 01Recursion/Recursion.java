9969a4592af7a2617185554c2ad3d909c2dee7f3
Sammy Almawaldi <almawaldi@hotmail.com>
2016-02-08 17:29:41 -0500

    Homework complete, testing interface compliance

--- 01Recursion/Recursion.java
+++ 01Recursion/Recursion.java
@@ -1,10 +1,31 @@
 //Windows NT 10.0.10586 "10"
 public class Recursion{
+
 	public static void main(String[]args){
 		Recursion x = new Recursion();
 		System.out.println(x.name());
+		int test = 10;
+		System.out.println("Guess:" + x.sqrt(100));
+		System.out.println("Actual: 10");
 	}
 	public String name(){
 		return "Almawaldi,Sammy";
+	}
+
+	public double sqrt(double n){
+		return sqrtFormula(n, 1);
+	}
+
+	private double sqrtFormula(double n, double guess){
+		if(percentError(Math.pow(guess, 2), n) < 0.000001){
+			return guess;
+		}
+		else{
+			return sqrtFormula(n, (n / guess + guess) / 2);
+		}
+	}
+
+	private double percentError(double guess, double actual){
+		return Math.abs((guess - actual) / guess) * 100;
 	}
 }
