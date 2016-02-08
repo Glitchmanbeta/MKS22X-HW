688438a1a08c8ce609bdcdf1ddb22b454163a9ad
Sammy Almawaldi <almawaldi@hotmail.com>
2016-02-08 17:02:36 -0500

    Name method complete

---
+++ 01Recursion/Recursion.java
@@ -0,0 +1,10 @@
+//Windows NT 10.0.10586 "10"
+public class Recursion{
+	public static void main(String[]args){
+		Recursion x = new Recursion();
+		System.out.println(x.name());
+	}
+	public String name(){
+		return "Almawaldi,Sammy";
+	}
+}
