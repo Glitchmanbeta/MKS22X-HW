2f74dd8681737a1b2531cb8b6f38772e80364834
Sammy Almawaldi <almawaldi@hotmail.com>
2016-02-23 21:17:06 -0500

    Made the scanner, file, and put the numbers into variables.

--- 04USACO/Bronze.java
+++ 04USACO/Bronze.java
@@ -2,17 +2,44 @@ import java.util.Scanner;
 import java.io.File;
 
 public class Bronze{
-    private boolean DEBUG = true;
+
+	private Scanner sc, an;
+	private int row, col, ele, commands;
+	private File f;
+	private static boolean DEBUG;
+	private int[][] lake;
 
     public static void main(String[]args){
-	    if(DEBUG){
-	     System.out.println("Happening");
-	    }
+    	DEBUG = true;
+	    Bronze x = new Bronze();
     }
 
     public Bronze(){
-	    Scanner sc = new Scanner(new File("makelake.in"));
-	    
+    	try{
+    		f = new File("makelake.in");
+	    	sc = new Scanner(f);
+	    	an = new Scanner(sc.nextLine());
+	    	row = an.nextInt();
+	    	col = an.nextInt();
+	    	ele = an.nextInt();
+	    	commands = an.nextInt();
+	    	if(DEBUG){
+	    		System.out.println(row + ", " + col + ", " + ele + ", " + commands);
+	    	}
+	    	lake = new int[row][col];
+	    	for(int i = 0; i < row; i++){
+	    		an = new Scanner(sc.nextLine());
+	    		for(int j = 0; j < col; j++){
+	    			lake[i][j] = an.nextInt();
+	    			if(DEBUG){
+	    				System.out.println(lake[i][j] + " ");
+	    			}
+	    		}
+	    	}
+	    }
+	    catch(Exception e){
+	    	System.out.println("File Not Found.");
+	    }
     }
 }
 
