3ec931770fdc05de80830fd17cb72a8a50b9c4f2
Sammy Almawaldi <win9xfreak@aol.com>
2016-02-28 21:15:43 +0000

    Complete

--- 05Maze/Maze.java
+++ 05Maze/Maze.java
@@ -108,7 +108,7 @@ public class Maze{
         return false; //so it compiles
     }
     
-    public String toString(){
+    /*public String toString(){
         String s = "";
         for(int i = 0; i < maze.length; i++){
             for(int j = 0; j < maze[i].length; j++){
@@ -117,6 +117,59 @@ public class Maze{
             s = s + "\n";
         }
         return s;
+    }*/
+    
+    //FREE STUFF!!! *you should be aware of this*
+
+    public void clearTerminal(){
+        System.out.println(CLEAR_SCREEN);
     }
+
+    public String toString(){
+        int maxx = maze.length;
+        int maxy = maze[0].length;
+        String ans = "";
+        if(animate){
+            ans = "Solving a maze that is " + maxx + " by " + maxy + "\n";
+        }
+        for(int i = 0; i < maxx * maxy; i++){
+            if(i % maxx == 0 && i != 0){
+                ans += "\n";
+            }
+            char c =  maze[i % maxx][i / maxx];
+            if(c == '#'){
+                ans += color(38,47)+c;
+            }else{
+                ans += color(33,40)+c;
+            }
+        }
+        return HIDE_CURSOR + go(0,0) + ans + "\n" + SHOW_CURSOR + color(37,40);
+    }
+
+    //MORE FREE STUFF!!! *you can ignore all of this*
+    //Terminal keycodes to clear the terminal, or hide/show the cursor
+    private static final String CLEAR_SCREEN =  "\033[2J";
+    private static final String HIDE_CURSOR =  "\033[?25l";
+    private static final String SHOW_CURSOR =  "\033[?25h";
+    //terminal specific character to move the cursor
+    private String go(int x,int y){
+        return ("\033[" + x + ";" + y + "H");
+    }
+
+    private String color(int foreground,int background){
+        return ("\033[0;" + foreground + ";" + background + "m");
+    }
+
+    private void wait(int millis){
+        try {
+            Thread.sleep(millis);
+        }
+        catch (InterruptedException e) {
+        }
+    }
+
+    
+    //END FREE STUFF
+
   
 }
