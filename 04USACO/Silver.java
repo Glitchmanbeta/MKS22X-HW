bc736afa995df407e465f9171b0291654d74ecb4
Sammy Almawaldi <win9xfreak@aol.com>
2016-02-29 17:33:45 +0000

    Began the main

--- 04USACO/Silver.java
+++ 04USACO/Silver.java
@@ -1,6 +1,13 @@
 public class Silver{
     
+    private boolean DEBUG;
+    private int n, m, t, r1, c1, r2, c2;
+    
     public static void main(String[]args){
-        System.out.println("IT'S NO USEEEEEEE!");
+        DEBUG = true;
+        if(DEBUG){
+            System.out.println("IT'S NO USEEEEEEE!");
+        }
+        Silver pothead = new Silver();
     }
 }
