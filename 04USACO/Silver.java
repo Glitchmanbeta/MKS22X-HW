6d8cd7ec504a4abaf984ee75bf69211bcd79fcbf
Sammy Almawaldi <almawaldi@hotmail.com>
2016-03-02 00:00:08 -0500

    Pascal's Triangle?

--- 04USACO/Silver.java
+++ 04USACO/Silver.java
@@ -25,21 +25,32 @@ public class Silver{
     		n = porcupine.nextInt();
     		m = porcupine.nextInt();
     		t = porcupine.nextInt();
+            field = new char[n][m];
     		porcupine.nextLine();
     		for(int row = 0; row < n; row++){
+                String s = porcupine.nextLine();
     			for(int col = 0; col < m; col++){
-    				if(porcupine.hasNext()){
-    					String s = porcupine.next();
-    					System.out.println(s);
-    					//field[row][col] = s.charAt(0);
-    				}
+    				field[row][col] = s.charAt(col);
     			}
-    			porcupine.nextLine();
-    		}
-    		//System.out.println(porcupine.toString());
+    	    }
+            if(DEBUG){
+                String s = "";
+                for(int row = 0; row < field.length; row++){
+                    for(int col = 0; col < field[row].length; col++){
+                        s = s + field[row][col] + " ";
+                    }
+                    s = s + "\n";
+                }
+                System.out.println(s);
+            }
+            r1 = porcupine.nextInt();
+            c1 = porcupine.nextInt();
+            r2 = porcupine.nextInt();
+            c2 = porcupine.nextInt();
+            System.out.println(r1 + " " + c1 + " " + r2 + " " + c2);
     	}
     	catch(FileNotFoundException e){
-    		System.out.println("IT'S NOT USEEEEEEE!\nBTW, your file isn't in the right location. The program kinda won't work without it :/");
+    		System.out.println("IT'S NO USEEEEEEE!\nBTW, your file isn't in the right location or isn't named correctly. The program kinda won't work without it :/");
     	}
     }
 }
