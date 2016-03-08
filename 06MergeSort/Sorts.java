5957d370d8c6cac3d32e9e61f8e8bea091f7544a
Sammy Almawaldi <win9xfreak@aol.com>
2016-03-08 17:34:05 +0000

    The beginnings of... idek

--- 06MergeSort/Sorts.java
+++ 06MergeSort/Sorts.java
@@ -1,11 +1,27 @@
 public class Sorts{
     
+    private static boolean DEBUG = true;
+    
     public static void main(String[]args){
-        System.out.println("Hello from the Dark Side");
-        System.out.println(name());
+        if(DEBUG){
+            System.out.println("Hello from the Dark Side");
+            System.out.println(name());
+        }
     }
     
     public static String name(){
         return "6,Almawaldi,Sammy";
+    }
+    
+    public static void mergesort(int[] data){
+        mergesort(data, 0, data.length - 1);
+    }
+    
+    private int[] mergesort(int[] data, int left, int right){
+        return data;
+    }
+    
+    private int[] merge(l1, r1, l2, r2){
+        
     }
 }
