e5e7b626b44856c93d93940496bfdb36b6df73b6
Sammy Almawaldi <win9xfreak@aol.com>
2016-03-08 17:28:11 +0000

    Name function finished

--- 06MergeSort/Sorts.java
+++ 06MergeSort/Sorts.java
@@ -2,6 +2,10 @@ public class Sorts{
     
     public static void main(String[]args){
         System.out.println("Hello from the Dark Side");
+        System.out.println(name());
     }
     
+    public static String name(){
+        return "6,Almawaldi,Sammy";
+    }
 }
