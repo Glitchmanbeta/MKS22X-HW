49a285c9325a83f1ca62d2c5f5896530b6749867
Sammy Almawaldi <almawaldi@hotmail.com>
2016-03-10 14:21:17 -0500

    Initial commit

---
+++ 07Quick/Quick.java
@@ -0,0 +1,10 @@
+public class Quick{
+
+	public static boolean DEBUG = true;
+
+	public static void main(String[] args){
+		if(DEBUG){
+			System.out.println("Gotta fix dem der Winders.");
+		}
+	}
+}
