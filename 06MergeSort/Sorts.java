e9269176a088e0afa24f274cd0191009b1d97961
Sammy Almawaldi <almawaldi@hotmail.com>
2016-03-09 23:30:27 -0500

    Merge Works, now to write the Mergesort

--- 06MergeSort/Sorts.java
+++ 06MergeSort/Sorts.java
@@ -6,6 +6,9 @@ public class Sorts{
         if(DEBUG){
             System.out.println("Hello from the Dark Side");
             System.out.println(name());
+            int[] test1 = {19, 44, 49, 52, 90};
+            int[] test2 = {7, 9, 20, 50, 51};
+            merge(test1, test2);
         }
     }
     
@@ -17,11 +20,42 @@ public class Sorts{
         mergesort(data, 0, data.length - 1);
     }
     
-    private int[] mergesort(int[] data, int left, int right){
+    private static int[] mergesort(int[] data, int left, int right){
         return data;
     }
     
-    private int[] merge(l1, r1, l2, r2){
-        
+    private static int[] merge(int[] ary1, int[] ary2){
+        int l = 0;
+        int r = 0;
+        int i = 0;
+        int[] merged = new int[ary1.length + ary2.length];
+        while(l < ary1.length && r < ary2.length){
+            if(ary1[l] <= ary2[r]){
+                merged[i] = ary1[l];
+                l++;
+                i++;
+            }
+            if(ary2[r] < ary1[l]){
+                merged[i] = ary2[r];
+                r++;
+                i++;
+            }
+        }
+        while(l < ary1.length){
+            merged[i] = ary1[l];
+            l++;
+            i++;
+        }
+        while(r < ary2.length){
+            merged[i] = ary2[r];
+            r++;
+            i++;
+        }
+        if(DEBUG){
+            for(int x = 0; x < merged.length; x++){
+                System.out.println(merged[x]);
+            }
+        }
+        return merged;
     }
 }
