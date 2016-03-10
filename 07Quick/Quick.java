7cdbbbe7756ad9f82c7c5824177383f036ac2692
Sammy Almawaldi <almawaldi@hotmail.com>
2016-03-10 14:32:25 -0500

    Added name method

--- 07Quick/Quick.java
+++ 07Quick/Quick.java
@@ -4,7 +4,15 @@ public class Quick{
 
 	public static void main(String[] args){
 		if(DEBUG){
-			System.out.println("Gotta fix dem der Winders.");
+			System.out.println("Gotta fix dem der Winders");
 		}
+	}
+
+	public static String name(){
+		return "6,Almawaldi,Sammy";
+	}
+
+	private static int partition(int[] data, int left, int right){
+
 	}
 }
