ddcdf1be01640f7f3dae3d308d986b978e7a4305
Sammy Almawaldi <almawaldi@hotmail.com>
2016-03-19 18:15:34 -0400

    Constructor works

--- 08MyLinkedList/MyLinkedList.java
+++ 08MyLinkedList/MyLinkedList.java
@@ -2,13 +2,28 @@ public class MyLinkedList{
     
     private LNode start;
     private int size;
+    private LNode current;
+    private static boolean DEBUG = true;
     
     public static void main(String[] args){
         System.out.println("Guess I got a bad habit");
+        MyLinkedList test = new MyLinkedList(4);
+    }
+
+    public MyLinkedList(int x){
+        start = new LNode(x);
+        size = 1;
+        current = start;
+        if(DEBUG){
+            System.out.println(size);
+            System.out.println(x);
+        }
     }
     
     public boolean add(int value){
-        return false;
+        current.next = new LNode(value);
+        size++;
+        return true;
     }
     
     public String toString(){
@@ -24,7 +39,16 @@ public class MyLinkedList{
     }
     
     private class LNode{
-        int value;
-        LNode next;
+        private int value;
+        private LNode next;
+
+        public LNode(int x, LNode y){
+            value = x;
+            next = y;
+        }
+
+        public LNode(int x){
+            value = x;
+        }
     }
 }
