04e2346b124ee1c72590cddb0698836137866b70
Sammy Almawaldi <almawaldi@hotmail.com>
2016-03-20 03:03:42 -0400

    NullPointerexception

--- 08MyLinkedList/MyLinkedList.java
+++ 08MyLinkedList/MyLinkedList.java
@@ -2,32 +2,41 @@ public class MyLinkedList{
     
     private LNode start;
     private int size;
-    private LNode current;
-    private static boolean DEBUG = true;
-    
+    private LNode end;
+
     public static void main(String[] args){
         System.out.println("Guess I got a bad habit");
-        MyLinkedList test = new MyLinkedList(4);
+        MyLinkedList test = new MyLinkedList(0);
+        System.out.println(test.toString());
+        test.add(1);
+        System.out.println(test.toString());
     }
 
     public MyLinkedList(int x){
         start = new LNode(x);
+        end = start.getNext();
+        System.out.println(end);
         size = 1;
-        current = start;
-        if(DEBUG){
-            System.out.println(size);
-            System.out.println(x);
-        }
     }
     
     public boolean add(int value){
-        current.next = new LNode(value);
+        end = new LNode(value);
         size++;
+        end = end.getNext();
+        System.out.println(end);
         return true;
     }
     
     public String toString(){
-        return "Nope.";
+        String s = "[";
+        LNode current = start;
+        for(int i = 0; i < size; i++){
+            System.out.println(s);
+            s = s + current.getValue() + " ";
+            current = current.getNext();
+        }
+        s += "]";
+        return s;
     }
     
     public int get(int index){
@@ -42,13 +51,16 @@ public class MyLinkedList{
         private int value;
         private LNode next;
 
-        public LNode(int x, LNode y){
-            value = x;
-            next = y;
-        }
-
         public LNode(int x){
             value = x;
+        }
+
+        public int getValue(){
+            return value;
+        }
+
+        public LNode getNext(){
+            return next;
         }
     }
 }
