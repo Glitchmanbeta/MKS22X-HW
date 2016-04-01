932d6dc667c794bd088bdae1cf257372e6a79155
Sammy Almawaldi <almawaldi@hotmail.com>
2016-04-01 12:09:50 -0400

    toString()

--- 10MyDeque/MyDeque.java
+++ 10MyDeque/MyDeque.java
@@ -1,6 +1,28 @@
-//0.0.0003
-public class MyDeque{
+//0.0.0019
+public class MyDeque<T>{ //MyDeck
+
+    private T[] Deque;
+    private int size, start, end;
+
     public static void main(String[] args){
-	System.out.println("And the bum rush begins...");
+	System.out.println("And the bum rush begins..."); //Compile Check
+    }
+    
+    /*private T[] grow(T[] data){
+	T[] bigger = new T[data.length * 2];
+	for(int i =
+	}*/
+
+    public String toString(){
+	String s = "[";
+	for(int i = 0; i < Deque.length; i++){
+	    if(i != size - 1){
+		s = s + Deque[i] + " ";
+	    }
+	    else{
+		s = s + Deque[i] + "]";
+	    }
+	}
+	return s;
     }
 }
