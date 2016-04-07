b375c17218c51308703c6dba1961c6992b24abdb
Sammy Almawaldi <almawaldi@hotmail.com>
2016-04-07 14:52:06 -0400

    Main and About

--- 09StackQueue/ParenDemo.java
+++ 09StackQueue/ParenDemo.java
@@ -1,6 +1,17 @@
 public class ParenDemo{
 	
 	public static void main(String[] args){
-		System.out.println("Up-Up-Down-Down-Left-Right-Left-Right-B-A-Start");
+		String input = "()()(([[]]))";
+		if(args.length > 0){
+			input = args[0];
+			System.out.println( isMatching(input)); 
+		}else{
+			System.out.println("Usage:"); 
+			System.out.println("java ParenDemo \"text\""); 
+		}
+	}
+
+	public static void about(){
+		System.out.println("ParenDemo by Sammy Almawaldi\nWritten on 7 April 2016\nVersion 0.1.0");
 	}
 }
