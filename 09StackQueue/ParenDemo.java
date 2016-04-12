bb4d742d16a31d2cfeee632e76a5b6a8f2c3ee80
Sammy Almawaldi <almawaldi@hotmail.com>
2016-04-12 12:10:09 -0400

    It compiles... but does it work???

--- 09StackQueue/ParenDemo.java
+++ 09StackQueue/ParenDemo.java
@@ -14,4 +14,57 @@ public class ParenDemo{
 	public static void about(){
 		System.out.println("ParenDemo by Sammy Almawaldi\nWritten on 7 April 2016\nVersion 0.1.0");
 	}
+
+    public static boolean isMatching(String s){
+	MyStack<Character> stack = new MyStack<Character>();
+	for(int i = 0; i < s.length(); i++){
+	    if(s.charAt(i) == '{' || 
+	       s.charAt(i) == '[' || 
+	       s.charAt(i) == '<' || 
+	       s.charAt(i) == '(' ||
+	       s.charAt(i) == '}' ||
+	       s.charAt(i) == ']' ||
+	       s.charAt(i) == '>' ||
+	       s.charAt(i) == ')'){
+		stack.push(s.charAt(i));
+	    }
+	}
+	while(stack.size() > 0){
+	    if(stack.size() % 2 == 1){
+		return false;
+	    }else{
+		if(stack.pop() == '}'){
+		    if(stack.peek() == '{'){
+			stack.pop();
+		    }else{
+			return false;
+		    }
+		}
+		else if(stack.pop() == ']'){
+		    if(stack.peek() == '['){
+			stack.pop();
+		    }else{
+			return false;
+		    }
+		}
+		else if(stack.pop() == '>'){
+		    if(stack.peek() == '<'){
+			stack.pop();
+		    }
+		    else{
+			return false;
+		    }
+		}
+		else if(stack.pop() == ')'){
+		    if(stack.peek() == '('){
+			stack.pop();
+		    }
+		    else{
+			return false;
+		    }
+		}
+	    }
+	}
+	return true;
+    }
 }
