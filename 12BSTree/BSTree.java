203456bb039778727405396e9995bbe18e49ad50
Sammy Almawaldi <almawaldi@hotmail.com>
2016-04-20 12:20:08 -0400

    Nodes and things

--- 12BSTree/BSTree.java
+++ 12BSTree/BSTree.java
@@ -1,6 +1,30 @@
 public class BSTree<T extends Comparable<T>>{
 
+    Node root;
+
     public static void main(String[] bs){
 	System.out.println("This tree is BS! (Short for Bob Saget)");
+    }
+
+    public class Node{
+	T value;
+	Node left, right;
+
+	public T getValue(){
+	    return value;
+	}
+
+	public void setValue(T data){
+	    value = data;
+	}
+
+	public Node getLeft(){
+	    return left;
+	}
+
+	public Node setLeft(Node x){
+	    left = x;
+	}
+
     }
 }
