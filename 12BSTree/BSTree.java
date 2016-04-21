2f8b49878d98e616978abf9a0892c9aa90bbffff
Sammy Almawaldi <almawaldi@hotmail.com>
2016-04-21 12:20:20 -0400

    This tree has some BS height

--- 12BSTree/BSTree.java
+++ 12BSTree/BSTree.java
@@ -3,13 +3,16 @@ public class BSTree<T extends Comparable<T>>{
     Node root;
 
     public static void main(String[] bs){
-	System.out.println("This tree is BS! (Short for Bob Saget)");
+	System.out.println("This tree is BS!");
     }
 
     public class Node{
 	T value;
 	Node left, right;
 
+	public Node(T x){
+	    value = x;
+	}
 	public T getValue(){
 	    return value;
 	}
@@ -22,9 +25,35 @@ public class BSTree<T extends Comparable<T>>{
 	    return left;
 	}
 
-	public Node setLeft(Node x){
+	public void setLeft(Node x){
 	    left = x;
 	}
 
+	public Node getRight(){
+	    return right;
+	}
+
+	public void setRight(Node x){
+	    right = x;
+	}
+
+	public int height(){
+	    if(left != null && right != null){
+		return left.height() + right.height() + 1;
+	    }
+	    else if(left != null && right == null){
+		return left.height() + 1;
+	    }
+	    else if(left == null && right != null){
+		return right.height() + 1;
+	    }
+	    else{
+		return 1;
+	    }
+	}
+	
+	public void add(T value){
+	    
+	}
     }
 }
