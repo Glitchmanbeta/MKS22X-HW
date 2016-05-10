e3ee11fed5d32f524436490237fb1fd6d9186694
Sammy Almawaldi <almawaldi@hotmail.com>
2016-05-10 12:04:33 -0400

    Sanic Speed

---
+++ 14Median/RunningMedian.java
@@ -0,0 +1,8 @@
+import java.util.NoSuchElementException;
+
+public class RunningMedian{
+    
+    public static void main(String[] medians){
+	System.out.println("GOTTA GO FAAAAAAAAAAAAAAAAAAAAAAAAAAST");
+    }
+}
